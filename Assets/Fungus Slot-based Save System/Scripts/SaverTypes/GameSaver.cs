3003d83729a36d0acc9697585eafb0fd0fd11022
Pavel Zobov <61383073+Pavkv@users.noreply.github.com>
2025-08-04 21:17:32 -0400

    Added handling for the narrative log data

---
+++ Assets/Fungus Slot-based Save System/Scripts/SaverTypes/GameSaver.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+using System.IO;
+
+namespace CGTUnity.Fungus.SaveSystem
+{
+    /// <summary>
+    /// This encodes game objects into save data for a whole playthrough, so their state 
+    /// can be restored upon loading the game.
+    /// Handles Flowcharts and NarrativeLogs by default.
+    /// To extend this to handle other data types, just modify or subclass this component.
+    /// </summary>
+    public class GameSaver: DataSaver<GameSaveData>, ISaveCreator<GameSaveData>
+    {
+        protected List<DataSaver> subsavers = new List<DataSaver>();
+        [SerializeField] Camera mainCamera;
+
+        #region Methods
+        protected virtual void Awake()
+        {
+           subsavers.AddRange(GetComponents<DataSaver>());
+           subsavers.RemoveAll(saver => saver == this); // This can't be its own subsaver!
+        }
+
+        public override IList<SaveDataItem> CreateItems()
+        {
+            // Create GameSaveData, and encode it into a SaveDataItem
+            var gameSave = CreateSave();
+            var jsonSave = JsonUtility.ToJson(gameSave, true);
+            var newItem = new SaveDataItem(saveType.Name, jsonSave);
+
+            // The array has only one element, since we only made one GameSaveData
+            return new SaveDataItem[1] {newItem};
+        }
+
+        /// <summary>
+        /// Creates and returns save data for the whole game.
+        /// </summary>
+        public virtual GameSaveData CreateSave()
+        {
+            var sceneName = SceneManager.GetActiveScene().name;
+            var newGameSave = new GameSaveData(sceneName, -1);
+            EncodeInto(ref newGameSave);
+            newGameSave.UpdateTime();
+            if (ProgressMarker.latestExecuted != null)
+                newGameSave.ProgressMarkerKey = ProgressMarker.latestExecuted.Key;
+
+            // It's common to make VN save file descs be the text that was in the textbox, 
+            // at the time of the save being made.
+            var description = "";
+            var sayDialog = SayDialog.ActiveSayDialog;
+
+            if (sayDialog != null && !string.IsNullOrEmpty(sayDialog.StoryText))
+                description = sayDialog.StoryText;
+           
+            newGameSave.Description = description;
+            
+            var narrativeLog = FungusManager.Instance.NarrativeLog;
+            if (narrativeLog != null)
+                newGameSave.narrativeLogJson = narrativeLog.GetJsonHistory();
+            
+            return newGameSave;
+        }
+
+        /// <summary>
+        /// Creates and returns save data with the passed slot number.
+        /// </summary>
+        public virtual GameSaveData CreateSave(int slotNumber)
+        {
+            var newGameSave = CreateSave();
+            newGameSave.SlotNumber = slotNumber;
+
+            RenderTexture rt = new RenderTexture(mainCamera.pixelWidth, mainCamera.pixelHeight, 24);
+            mainCamera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+
+            mainCamera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            screenShot.Apply();
+
+            mainCamera.targetTexture = null;
+            RenderTexture.active = null;
+            Destroy(rt);
+
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = Path.Combine(Application.persistentDataPath, "screen_" + slotNumber + ".png");
+            File.WriteAllBytes(filename, bytes);
+            
+            var narrativeLog = FungusManager.Instance.NarrativeLog;
+            if (narrativeLog != null)
+                newGameSave.narrativeLogJson = narrativeLog.GetJsonHistory();
+
+            newGameSave.ScreenshotPath = filename;
+            return newGameSave;
+        }
+
+        /// <summary>
+        /// Creates and returns save data for the whole game, set for the passed save slot.
+        /// </summary>
+        public virtual GameSaveData CreateSave(SaveSlot saveSlot)
+        {
+            return CreateSave(saveSlot.Number);
+        }
+
+        #region Helpers
+
+        protected virtual void EncodeInto(ref GameSaveData saveData)
+        {
+            for (int i = 0; i < subsavers.Count; i++)
+            {
+                var subsaver = subsavers[i];
+                var saveDataItems = subsaver.CreateItems();
+                saveData.Items.AddRange(saveDataItems);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+
+    }
+}
---
+++ Assets/Fungus Slot-based Save System/Scripts/UI/SaveSlot.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Globalization;
+using System.IO;
+using CGT.Globalization;
+
+namespace CGTUnity.Fungus.SaveSystem
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class SaveSlot : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] protected Text numDisplay = null;
+        [SerializeField] protected Text descDisplay = null;
+        [SerializeField] protected Text dateDisplay = null;
+        [SerializeField] protected StandardFormat dateFormat = StandardFormat.fullLongDate;
+        [SerializeField] protected bool refreshContinuously = true;
+        [SerializeField] protected RawImage Screenshot;
+        [SerializeField] protected int saveSlotNumber;
+
+        protected GameSaveData saveData = null;
+        protected Texture2D cachedTexture;
+
+        #endregion
+
+        #region Properties and helpers
+        public RectTransform rectTransform { get; private set; }
+
+        public virtual GameSaveData SaveData
+        {
+            get { return saveData; }
+            set
+            {
+                if (value == null)
+                {
+                    WarnForNullSaveDataAssignment();
+                    return;
+                }
+
+                saveData = value;
+                SyncWithSaveData();
+                UpdateDisplays();
+                Signals.SaveSlotUpdated.Invoke(this, value);
+            }
+        }
+
+        void WarnForNullSaveDataAssignment()
+        {
+            Debug.LogWarning("Cannot assign null GameSaveData to a Save Slot. Use Clear() instead.");
+        }
+
+        protected virtual void SyncWithSaveData()
+        {
+            //Number = saveData.SlotNumber;
+            Description = saveData.Description;
+            Date = saveData.LastWritten;
+            ScreenPath = saveData.ScreenshotPath;
+        }
+        
+        public virtual int Number
+        {
+            get => saveSlotNumber;
+            set => saveSlotNumber = value;
+        }
+        public virtual string Description { get; set; }
+        public virtual DateTime Date { get; set; }
+        public virtual string ScreenPath { get; set; }
+
+        public virtual void UpdateDisplays()
+        {
+            UpdateNumberDisplay();
+            UpdateDateDisplay();
+            UpdateDescriptionDisplay();
+            LoadScreenshot();
+        }
+
+        protected virtual void UpdateNumberDisplay()
+        {
+            numDisplay.text = "Save #" + Number;
+        }
+
+        protected virtual void UpdateDateDisplay()
+        {
+            if (Date == default(DateTime))
+                dateDisplay.text = "";
+            else
+                dateDisplay.text = Date.ToString(StandardFormatValues.vals[dateFormat], userLocale);
+        }
+
+        protected readonly CultureInfo userLocale = CultureInfo.CurrentUICulture;
+
+        protected virtual void UpdateDescriptionDisplay()
+        {
+            descDisplay.text = Regex.Replace(Description, "[^а-яА-Я ]", "");
+        }
+
+        private void LoadScreenshot()
+        {
+            if (Screenshot == null) return; // Prevent errors if not assigned
+
+            if (!File.Exists(ScreenPath))
+            {
+                Screenshot.enabled = false; // Hide if no screenshot is found
+                return;
+            }
+
+            byte[] imageBytes = File.ReadAllBytes(ScreenPath);
+            if (cachedTexture == null)
+                cachedTexture = new Texture2D(100, 100, TextureFormat.RGBA32, false);
+
+            if (cachedTexture.LoadImage(imageBytes))
+            {
+                Screenshot.texture = cachedTexture;
+                Screenshot.enabled = true;
+            }
+            else
+            {
+                Debug.LogWarning("Failed to load screenshot from " + ScreenPath);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        protected virtual void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            Description = "";
+            clickReceiver = GetComponent<Button>();
+            clickReceiver.onClick.AddListener(OnClick);
+            UpdateDisplays();
+        }
+
+        protected Button clickReceiver = null;
+
+        protected virtual void OnClick()
+        {
+            Signals.SaveSlotClicked.Invoke(this);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            clickReceiver.onClick.RemoveListener(OnClick);
+        }
+
+        public virtual void Clear()
+        {
+            saveData = null;
+            Description = "";
+            Date = default;
+            ScreenPath = "";
+            Screenshot.texture = null;
+            Screenshot.enabled = false;
+            UpdateDisplays();
+            Signals.SaveSlotUpdated.Invoke(this, SaveData);
+        }
+        #endregion
+    }
+}
