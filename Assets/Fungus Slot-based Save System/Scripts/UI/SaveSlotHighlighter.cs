5feb2e46829d6aea60c4c002c860085b62703188
Pavel Zobov <61383073+Pavkv@users.noreply.github.com>
2025-09-02 22:19:49 -0400

    New Class to handle the save slot highlights

---
+++ Assets/Fungus Slot-based Save System/Scripts/UI/SaveSlotHighlighter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace CGTUnity.Fungus.SaveSystem
+{
+    public class SaveSlotHighlighter : MonoBehaviour
+    {
+        public static SaveSlotHighlighter Instance { get; private set; }
+
+        private SaveSlot currentlySelectedSlot;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        public void SetSelectedSlot(SaveSlot slot)
+        {
+            if (currentlySelectedSlot != null)
+            {
+                currentlySelectedSlot.SetSelected(false);
+            }
+
+            currentlySelectedSlot = slot;
+            currentlySelectedSlot.SetSelected(true);
+        }
+    }
+}
